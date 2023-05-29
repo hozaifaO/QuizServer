c6737f0d7d59490374f93e8d4308c181a9ee68b6
hozaifaO <hozaifaowaisi.h@gmail.com>
2023-05-29 18:42:16 -0400

    Made the three initial entities and made sure they work with the local mysql database

---
+++ src/main/java/com/hozaifa/quizserver/models/UserRole.java
@@ -0,0 +1,15 @@
+package com.hozaifa.quizserver.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class UserRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userRoleId;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User user;
+    @ManyToOne
+    private Role role;
+
+}
