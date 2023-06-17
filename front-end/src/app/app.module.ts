2d2bdd266d71a751dc96b413a7b75fb3bd4b047e
hozaifaO <hozaifaowaisi.h@gmail.com>
2023-06-17 12:20:38 -0400

    Cleaned up

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import { NavbarComponent } from './general-components/navbar/navbar.component';
+import { FooterComponent } from './general-components/footer/footer.component';
+import { SignupComponent } from './page-components/signup/signup.component';
+import { LoginComponent } from './page-components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
